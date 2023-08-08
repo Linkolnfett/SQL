f86e361d2bd2d58b7eaafddce6a196c2b25d54ca
Linkolnfett <linkolnfett@gmail.com>
2023-08-08 17:49:18 +0300

    v 1.1

--- SQL_HW_3.sql
+++ SQL_HW_3.sql
@@ -1,12 +1,10 @@
 -- 1. Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами.
 select employee_name , monthly_salary from employees 
-		join roles_employee using (id)
-		join employee_salary using (employee_id)
-		join salary on employee_salary.id = salary.id;
+		join employee_salary on employee_id = employees.id 
+		join salary on salary_id = salary.id;	
 -- 2. Вывести всех работников у которых ЗП меньше 2000.
-select employee_name from employees 
-		join roles_employee on employees.id = employee_id
-		join employee_salary using (employee_id)
+select employee_name, monthly_salary from employees 
+		join employee_salary on employee_id = employees.id 
 		join salary on salary.id = employee_salary.salary_id
 where monthly_salary < 2000;
 -- 3. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
